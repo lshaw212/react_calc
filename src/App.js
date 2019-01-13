adc539f7da40d98f53ffc45450cf93611e908d20
lshaw212 <lshaw212@gmail.com>
2019-01-13 22:51:20 +0000

    Cleaning up input code for easier readability

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import Checker from './check';
 
 const operandSymbols = 'x÷+-'
 const Operators = '0123456789';
-let answer,value,equation, firstValue, secondValue;
+let value,equation;
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      answer: 0,
+      answer: '',
       equation: '',
       firstValue: '',
       secondValue: '',
@@ -24,41 +24,33 @@ class App extends Component {
   }
 
   inputNumber = name => {
-    if(!this.state.waitingForOperator){
-      value= this.state.secondValue + name;
-      equation = this.state.equation + name;
-      this.setState({secondValue:value,equation:equation});
-    } else {
-      value = this.state.firstValue + name;
-      equation = this.state.equation + name;
-      this.setState({firstValue:value,equation:equation});
-    }
+    const {firstValue, secondValue, equation, waitingForOperator} = this.state;
+    if(waitingForOperator)
+      return this.setState({firstValue:firstValue+name,equation:equation+name});
+    else 
+      return this.setState({secondValue:secondValue+name,equation:equation+name});
   }
 
   inputOperator = async name => {
-    value = this.state.equation[this.state.equation.length-1];
-    if(!isNaN(value) && this.state.waitingForOperator){
-      equation= this.state.equation + name;
-      return this.setState({equation:equation, waitingForOperator:false, operator:name});
-    } else if(!isNaN(value) && !this.state.waitingForOperator) {
+    const {operator, firstValue, secondValue, answer} = this.state;
+    if(!operator){
+      if(!firstValue && answer)
+        return this.setState({firstValue:answer,operator:name, equation:answer + name, waitingForOperator:false});
+      else if(!firstValue && !answer)
+        return this.setState({firstValue:0,operator:name, equation:0 + name, waitingForOperator:false});
+      else
+        this.setState({operator:name, equation:firstValue + name, waitingForOperator:false});
+    } else {
+      console.log("we have operator")
       await this.inputEquals();
-      equation = this.state.answer + name;
-      return this.setState({waitingForOperator:false,firstValue:this.state.answer,operator:name,equation:equation});
-    } else if(!value){
-      if(this.state.answer === 0){
-        equation = 0 + name;
-        return this.setState({equation:equation, firstValue:0,operator:name, waitingForOperator:true});
-      } else{
-        equation = this.state.answer + name;
-        return this.setState({equation:equation, firstValue:this.state.answer,operator:name, waitingForOperator:false});
-      }
+      return this.setState({firstValue:answer,operator:name,equation:answer+name, waitingForOperator:false});
     }
-    console.log("Fix when a dot is last");
-    return
   }
 
   inputEquals = () => {
-    value = Calculator(this.state.firstValue,this.state.secondValue,this.state.operator);
+    console.log("no?");
+    const {firstValue,secondValue,operator} = this.state;
+    value = Calculator(firstValue, secondValue, operator);
     this.resetState();
     if(value)
       this.setState({answer:value});
@@ -71,37 +63,38 @@ class App extends Component {
     }
   }
   inputDot = () => {
-    if(!this.state.waitingForOperator){
-      if(!this.state.secondValue.includes('.'))
+    const { firstValue, secondValue, waitingForOperator} = this.state;
+    if(!waitingForOperator){
+      if(!secondValue.includes('.'))
         return this.inputNumber('.');
     } else {
-      if(!this.state.firstValue.includes('.'))
+      if(!firstValue.includes('.'))
         return this.inputNumber('.');
     }
   }
   inputDelete = () => {
-    let lastChar = this.state.equation[this.state.equation.length-1];
-    equation = this.state.equation.slice(0,-1);
+    const {equation,secondValue,firstValue,waitingForOperator} = this.state;
+    let lastChar = equation[equation.length-1];
+    let slice = equation.slice(0,-1);
     if(operandSymbols.includes(lastChar))
-      return this.setState({operator:null,waitingForOperator:true,equation:equation})
-    if(!this.state.waitingForOperator){
-      value = this.state.secondValue.slice(0,-1);
-      return this.setState({secondValue:value,equation:equation});
+      return this.setState({operator:null,waitingForOperator:true,equation:slice})
+    if(!waitingForOperator){
+      value = secondValue.slice(0,-1);
+      return this.setState({secondValue:value,equation:slice});
     } else {
-      value = this.state.firstValue.slice(0,-1);
-      return this.setState({firstValue:value,equation:equation});
+      value = firstValue.slice(0,-1);
+      return this.setState({firstValue:value,equation:slice});
     }
   }
   changeSign = () => {
-    if(this.state.waitingForOperator)
-    {
-      firstValue = Number(this.state.firstValue) * -1;
-      equation = firstValue;
-      return this.setState({firstValue:firstValue,equation:equation});
+    const {firstValue,secondValue,operator,waitingForOperator} = this.state;
+    let calc;
+    if(waitingForOperator){
+      calc = Number(firstValue) * -1;
+      return this.setState({firstValue:calc,equation:calc});
     } else {
-      secondValue = Number(this.state.secondValue) * -1;
-      equation = this.state.firstValue + this.state.operator + secondValue;
-      return this.setState({secondValue:secondValue,equation:equation});
+      calc = Number(secondValue) * -1;
+      return this.setState({secondValue:calc,equation:firstValue+operator+calc});
     }
   }
 
@@ -210,4 +203,29 @@ export default App;
 //     equation = this.state.answer + name;
 //     return this.setState({waitingForOperand:true,firstValue:this.state.answer,operator:name,equation:equation});
 //   }
+// }
+
+
+
+
+// inputOperator = async name => {
+//   value = this.state.equation[this.state.equation.length-1];
+//   if(!isNaN(value) && this.state.waitingForOperator){
+//     equation= this.state.equation + name;
+//     return this.setState({equation:equation, waitingForOperator:false, operator:name});
+//   } else if(!isNaN(value) && !this.state.waitingForOperator) {
+//     await this.inputEquals();
+//     equation = this.state.answer + name;
+//     return this.setState({waitingForOperator:false,firstValue:this.state.answer,operator:name,equation:equation});
+//   } else if(!value){
+//     if(this.state.answer === 0){
+//       equation = 0 + name;
+//       return this.setState({equation:equation, firstValue:0,operator:name, waitingForOperator:true});
+//     } else{
+//       equation = this.state.answer + name;
+//       return this.setState({equation:equation, firstValue:this.state.answer,operator:name, waitingForOperator:false});
+//     }
+//   }
+//   console.log("Fix when a dot is last");
+//   return
 // }

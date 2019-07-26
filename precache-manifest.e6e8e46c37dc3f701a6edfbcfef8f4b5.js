90c06b1c4caf33f7948b42f37154598847902ca9
lshaw212 <lshaw212@gmail.com>
2019-07-26 18:56:40 +0100

    Updates

---
+++ precache-manifest.e6e8e46c37dc3f701a6edfbcfef8f4b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf68caa5cfb1fd5c6986bad60e209315",
+    "url": "/react_calc/index.html"
+  },
+  {
+    "revision": "62419dd42e4a96f5adcf",
+    "url": "/react_calc/static/css/main.293fcb5b.chunk.css"
+  },
+  {
+    "revision": "ff95bf81788eadeb1b39",
+    "url": "/react_calc/static/js/2.5058da69.chunk.js"
+  },
+  {
+    "revision": "62419dd42e4a96f5adcf",
+    "url": "/react_calc/static/js/main.b50f9264.chunk.js"
+  },
+  {
+    "revision": "6a517be6f380a773b8e8",
+    "url": "/react_calc/static/js/runtime~main.440362f8.js"
+  },
+  {
+    "revision": "db9b78400cc727ef0b4bdf5b0e6be6b5",
+    "url": "/react_calc/static/media/LEDCalculator.db9b7840.ttf"
+  },
+  {
+    "revision": "45bbbb730da663d94808ebd6eeed93b8",
+    "url": "/react_calc/static/media/MGBOLD.45bbbb73.TTF"
+  }
+]);
